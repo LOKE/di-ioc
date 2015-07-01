b7a1ab6f2b93d22541926719edf67b9e35bfa9f3
Anthony Foster <git@af.id.au>
2015-07-01 23:04:32 +1000

    Simplify code

--- index.js
+++ index.js
@@ -1,65 +1,93 @@
 'use strict';
 
-module.exports = Router;
-module.exports.create = function () {
-  return new Router();
-};
+var $private = require('private').create();
+var annotate = require('fn-annotate');
 
-var $private = require('private').createAccessor();
+Router.create = module.exports = Router;
 
-var Container = require('./container');
+var TYPE_SINGLETON = 'singleton';
+var TYPE_TRANSIENT = 'transient';
+var TYPE_INTERFACE = 'interface';
+var TYPE_SUBROUTER = 'use';
+var NULL_INTERFACE = 'INTERFACE_NOT_IMPLEMENTED';
 
-function getArguments(fn) {
-  var injects = /^function *\(([^\)]*)\)/.exec(fn + '');
-  if (!injects) {
-    throw new Error('Could not parse function signature for injection dependencies.');
-  }
-
-  return injects[1].split(',').map(function (e) {
-    return e && e.trim();
-  })
-  .filter(Boolean);
-}
-
-function Router() {
+function Router(requireFn) {
+  if (!(this instanceof Router)) return new Router(requireFn);
+  $private(this).require = requireFn;
   $private(this).defs = [];
 }
 
-Router.prototype.define = function (name, defn, type) {
-  type = type || 'singleton';
-  $private(this).defs.push({name: name, defn: defn, type: type});
-  return this;
-};
-
-Router.prototype.singleton = function (name, defn) {
-  return this.define(name, defn, 'singleton');
-};
-
-Router.prototype.transient = function (name, defn) {
-  return this.define(name, defn, 'transient');
-};
-
-function attach(router, container, namespace) {
-  namespace = namespace || [];
-  $private(router).defs.forEach(function (def) {
-    if (typeof def.defn === 'function') {
-      var fn = def.defn;
-      var args = getArguments(fn);
-      container.define(namespace, def.name, def.type, args, function () {
-        var obj = {};
-        return fn.apply(obj, arguments) || obj;
-      });
-    } else if (def.defn instanceof Router) {
-      attach(def.defn, container, namespace.concat(def.name));
-    }
+function set(router, type, path, factory) {
+  factory = factory || $private(router).require(path);
+  var deps = typeof factory === 'function' && annotate(factory);
+  var name = path.replace('./', '').replace(/-(.)/g, function (s, first) {
+    return first.toUpperCase();
   });
+
+  $private(router).defs.push({name: name, type: type, factory: factory, deps: deps});
 }
 
+[TYPE_SINGLETON, TYPE_TRANSIENT, TYPE_INTERFACE, TYPE_SUBROUTER]
+.forEach(function (type) {
+  Router.prototype[type] = function (path, fn) { set(this, type, path, fn); return this; };
+});
+
+Router.prototype.build = function (getters) {
+  var instance = {};
+  $private(this).defs.forEach(function (def) {
+    if (def.type === TYPE_SUBROUTER) return instance[def.name] = def.factory.build(getters);
+    if (def.type === TYPE_INTERFACE) return  getters[def.name] = NULL_INTERFACE;
+
+    def.deps.forEach(function (name) {
+      if (!getters[name]) throw new Error('Unknown dependency: ' + name + ' required by ' + def.name); }
+    );
+
+    var sharedInstance;
+    var get = getters[def.name] = function () {
+      if (sharedInstance) return sharedInstance;
+
+      var args = def.deps.map(function (name) { return getters[name](); });
+
+      var newInstance = {};
+      newInstance = def.factory.apply(newInstance, args) || newInstance;
+
+      if (def.type === TYPE_SINGLETON) {
+        sharedInstance = newInstance;
+      }
+
+      if (newInstance.stop) {
+        if (def.type === TYPE_TRANSIENT) {
+          throw new Error('Service "' + def.name + '" can\'t have a .stop() method because it is transient.');
+        }
+        $private(getters).active.push(sharedInstance);
+      }
+
+      return newInstance;
+    };
+
+    Object.defineProperty(instance, def.name, { enumerable : true, get: get});
+  });
+  return instance;
+};
+
 Router.prototype.init = function () {
-  var container = new Container();
+  var active = [];
+  var state = {};
+  $private(state).active = active;
+  var instance = this.build(state);
+  instance.stop = function () {
+    var results = active.map(function (singletonService) {
+      if (singletonService.stop) return singletonService.stop();
+    }).filter(Boolean);
 
-  // Set up container with dependency graph.
-  attach(this, container, []);
+    active.splice(0, active.length);
+    return Promise.all(results);
+  };
+  return instance;
+};
 
-  return container.export();
+// Backwards compatibility alias:
+Router.prototype.define = function (name, defn) {
+  if (defn instanceof Router) return this.use(name, defn);
+  return this.singleton(name, defn);
 };
